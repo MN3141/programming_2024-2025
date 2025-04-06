d813800047ae34e5bedc400cac230213c7bcb977
Mihai Neag <mihaineag6@gmail.com>
2025-04-06 20:18:11 +0300

    Added template for MIPS applications

---
+++ MicroprocessorSystems/template/make.bat
@@ -0,0 +1,6 @@
+:: Enter the name of the assembly file in order for the script
+:: to work correctly
+
+@echo off
+
+call ..\utils.bat %cd%\%1
---
+++ MicroprocessorSystems/template/template.s
@@ -0,0 +1,15 @@
+# Notes:
+# 1. Use LF as line ending in order
+#    for the program to run.
+# 2. Make sure that after 'done'
+#    label there is a new line.
+
+.data
+    .align 2 # align with 2^x bytes
+
+.text
+
+    loop:
+
+    done:
+
