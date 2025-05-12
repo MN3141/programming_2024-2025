874d34dfef1ef5d42a4c5b6d1eedc506a8c4fa9b
Mihai Neag <mihaineag6@gmail.com>
2025-05-12 11:28:40 +0300

    Mutex sync

---
+++ OperatingSystems/FooWriter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Text;
+using WinAPI = WinApiClass.WinApiClass;
+
+namespace Foo
+{
+    internal static class Program
+    {
+
+        static void Main()
+        {
+
+            WinAPI.OpenMutex(0x001f001, false, "FooMutex");
+            string fileName = "C:\\Arhiva_ULBS\\Proiecte\\Anul_III\\programming_2024-2025\\OperatingSystems\\Mutex\\foo.txt";
+            IntPtr fileHandle = WinAPI.CreateFile(
+             fileName,
+             WinAPI.FileAccess.FILE_WRITE_DATA,        // Open for reading and writing
+             WinAPI.FileShare.None,              // Do not allow sharing
+             IntPtr.Zero,                 // No security attributes
+             WinAPI.FileMode.OPEN_EXISTING,             // Open existing
+             WinAPI.FileAttributes.Normal,        // Normal file attributes
+             IntPtr.Zero);
+
+            byte[] msg = Encoding.UTF8.GetBytes("IPRS BALGRAD. Mikro Avtamat OGAS Spejalni. 1453-2003");
+            uint bytesToWrite = (uint)msg.Length;
+            uint bufferSize = bytesToWrite;
+            if (!WinAPI.WriteFile(fileHandle, msg, bufferSize, out bytesToWrite, IntPtr.Zero))
+                throw new Exception("Cannot write to given file!");
+
+        }
+    }
+}
---
+++ OperatingSystems/Mutex/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using WinAPI = WinApiClass.WinApiClass;
+
+namespace Foo
+{
+    internal static class Program
+    {
+
+        static void Main()
+        {
+            string fileName = "C:\\Arhiva_ULBS\\Proiecte\\Anul_III\\programming_2024-2025\\OperatingSystems\\Mutex\\foo.txt";
+            IntPtr fileHandle = WinAPI.CreateFile(
+             fileName,
+             WinAPI.FileAccess.FILE_READ_DATA,        // Open for reading and writing
+             WinAPI.FileShare.None,              // Do not allow sharing
+             IntPtr.Zero,                 // No security attributes
+             WinAPI.FileMode.OPEN_EXISTING,             // Open existing
+             WinAPI.FileAttributes.Normal,        // Normal file attributes
+             IntPtr.Zero);
+
+            uint bufferSize = 1;
+            uint readBytes = 0;
+            byte[] fileBuffer = new byte[bufferSize];
+
+            IntPtr mutexHandler = WinAPI.CreateMutex(0, true, "FooMutex");
+            if (!WinAPI.ReadFile(fileHandle, fileBuffer, bufferSize, out readBytes, IntPtr.Zero))
+                throw new Exception("Cannot read given file!");
+            string content = System.Text.Encoding.UTF8.GetString(fileBuffer, 0, (int)readBytes);
+            Console.WriteLine(content);
+
+            WinAPI.ReleaseMutex(mutexHandler);
+        }
+    }
+}
